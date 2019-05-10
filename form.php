b6c0c1f700fa728a7cf2746fcd8302d249595207
Anastasia <ain.ovcharova@gmail.com>
2019-05-10 14:06:04 +0000

    Added ajax

--- form.php
+++ form.php
@@ -1,9 +1,5 @@
 <p>
-    Welcome: <?php echo $_POST["client-name"]; ?><br>
+    Welcome <?php echo $_POST["client-name"]; ?><br>
     Your Email: <?php echo $_POST["client-mail"]; ?><br>
     Your Message: <?php echo $_POST["client-message"]; ?>
-</p>
-
-<p>
-    Welcome <?php echo $_POST["mail"]; ?><br>
 </p>
--- js/index.js
+++ js/index.js
@@ -1,25 +1,29 @@
+const XHR_DONE = 4;
+const HTTP_OK = 200;
+
 document.querySelector('.form-client-data input[type=submit]').addEventListener('click', submitForm);
 
 function submitForm(e) {
     e.preventDefault();
     fetch('form.php', {
         method: 'POST',
-        body: new FormData(document.querySelector('.'))
+        body: new FormData(document.querySelector('.form-client-data'))
     })
      .then( response => response.text() )
      .then( html => document.querySelector('.server-response')
                                 .innerHTML = html );
 }
 
-document.querySelector('.client-email input[type=submit]').addEventListener('click', submitForm);
+document.querySelector('.get-shiba').addEventListener('click', getShiba);
 
-function submitForm(e) {
-    e.preventDefault();
-    fetch('form.php', {
-        method: 'POST',
-        body: new FormData(document.querySelector('.client-email'))
-    })
-     .then( response => response.text() )
-     .then( html => document.querySelector('.server-response')
-                                .innerHTML = html );
+function getShiba() {
+    const corsAnywhere = 'https://cors-anywhere.herokuapp.com/';
+	const apiUrl = 'http://shibe.online/api/shibes?count=1';
+    fetch(corsAnywhere + apiUrl)
+        .then( response => response.json() )
+        .then( shibas => {
+            document.querySelector('.shiba').innerHTML = '<img class="shiba-img"src="' + shibas[0] + '">';
+        });
 }
+
+getShiba();
