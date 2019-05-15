1280896508cdec2be8f5033a539dffe48d26b0cc
Anastasia <ain.ovcharova@gmail.com>
2019-05-15 13:18:32 +0000

    Fixed html iframe

--- js/index.js
+++ js/index.js
@@ -6,22 +6,22 @@ document.querySelector('.form-client-data input[type=submit]').addEventListener(
 function submitForm(e) {
     e.preventDefault();
     fetch('form.php', {
-        method: 'POST',
-        body: new FormData(document.querySelector('.form-client-data'))
-    })
-     .then( response => response.text() )
-     .then( html => document.querySelector('.server-response')
-                                .innerHTML = html );
+            method: 'POST',
+            body: new FormData(document.querySelector('.form-client-data'))
+        })
+        .then(response => response.text())
+        .then(html => document.querySelector('.server-response')
+            .innerHTML = html);
 }
 
 document.querySelector('.get-shiba').addEventListener('click', getShiba);
 
 function getShiba() {
     const corsAnywhere = 'https://cors-anywhere.herokuapp.com/';
-	const apiUrl = 'http://shibe.online/api/shibes?count=1';
+    const apiUrl = 'http://shibe.online/api/shibes?count=1';
     fetch(corsAnywhere + apiUrl)
-        .then( response => response.json() )
-        .then( shibas => {
+        .then(response => response.json())
+        .then(shibas => {
             document.querySelector('.shiba').innerHTML = '<img class="shiba-img"src="' + shibas[0] + '">';
         });
 }
